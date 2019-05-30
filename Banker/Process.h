0208dd4ad056a4b7d2051d26d7693f87262811dd
LTXX <ltxxgo@icloud.com>
2019-05-30 11:23:47 +0800

    Fix some bugs.

--- Banker/Process.h
+++ Banker/Process.h
@@ -12,16 +12,17 @@
 // 进程数量
 #define NumberOfProcesses 5
 
+// 资源结构体, 用变量名表示资源名, 变量值表示资源数量
 struct Resource{
     int A;
     int B;
     int C;
 };
 
-//记录安全序列
+// 记录安全序列
 int safe[NumberOfProcesses];
 
-//最大需求矩阵
+// 最大需求矩阵
 Resource Max[NumberOfProcesses] =
 {
     {7, 5, 3},
@@ -31,7 +32,7 @@ Resource Max[NumberOfProcesses] =
     {4, 3, 3}
 };
 
-//已分配资源数矩阵
+// 已分配资源矩阵
 Resource Allocation[NumberOfProcesses] =
 {
     {0, 1, 0},
@@ -41,7 +42,7 @@ Resource Allocation[NumberOfProcesses] =
     {0, 0, 2}
 };
 
-//需求矩阵
+// 需求矩阵
 Resource Need[NumberOfProcesses] =
 {
     {7, 4, 3},
@@ -51,8 +52,10 @@ Resource Need[NumberOfProcesses] =
     {4, 3, 1}
 };
 
+// 可用资源向量
 Resource Available = {3, 3, 2};
+
 Resource Work;
-bool Finish[NumberOfProcesses] = {false, false, false, false, false};
+bool Finish[NumberOfProcesses];
 
 #endif /* Process_h */
--- Banker/main.cpp
+++ Banker/main.cpp
@@ -8,164 +8,8 @@
 
 #include <iostream>
 #include "Process.h"
+#include "Banker.h"
 using namespace std;
-
-//试探分配
-void tryAllocate(int process, Resource resource)
-{
-    Available.A -= resource.A;
-    Available.B -= resource.B;
-    Available.C -= resource.C;
-    
-    Allocation[process].A += resource.A;
-    Allocation[process].B += resource.B;
-    Allocation[process].C += resource.C;
-    
-    Need[process].A -= resource.A;
-    Need[process].B -= resource.B;
-    Need[process].C -= resource.C;
-}
-
-//若试探分配后进入不安全状态，将分配回滚
-void rollBack(int process, Resource resource)
-{
-    Available.A += resource.A;
-    Available.B += resource.B;
-    Available.C += resource.C;
-    
-    Allocation[process].A -= resource.A;
-    Allocation[process].B -= resource.B;
-    Allocation[process].C -= resource.C;
-    
-    Need[process].A += resource.A;
-    Need[process].B += resource.B;
-    Need[process].C += resource.C;
-}
-
-//安全性检查
-bool safetyCheck()
-{
-    // 以下行用于输出表格, 可单独注释掉
-    // start
-    cout << "当前时刻的安全序列: " << endl;
-    cout << "+-----------------------------------------------------------------+" << endl;
-    cout << "|       |    Work   |   Need    |Allocation |Work+Alloca|        |" << endl;
-    cout << "|Process|-----------+-----------+-----------+-----------+ Finish |" << endl;
-    cout << "|       | A   B   C | A   B   C | A   B   C | A   B   C |        |" << endl;
-    cout << "|-------+-----------+-----------+-----------+-----------+--------|" << endl;
-    // end
-    
-    Work = Available;
-    // 每一次执行安全检查时都初始化 Finish 数组
-    for (int i = 0; i < NumberOfProcesses; i++) {
-        Finish[i] = false;
-    }
-    int securityNumber = 0;
-    
-    for (int i = 0; i < NumberOfProcesses; i++) {
-        //是否已检查过
-        if (Finish[i] == false) {
-            //是否有足够的资源分配给该进程
-            if (Need[i].A <= Work.A && Need[i].B <= Work.B && Need[i].C <= Work.C) {
-                // 有则使其执行完成，并将已分配给该进程的资源全部回收
-                Finish[i] = true;
-                
-                // 以下行用以输出安全序列表格中成功执行的 true 行, 可单独注释掉
-                // start
-                printf( "| P%d    | %d   %d   %d | %d   %d   %d | %d   %d   %d | %d   %d   %d |  %s  |\n",
-                       i,
-                       Work.A, Work.B, Work.C,
-                       Need[i].A, Need[i].B, Need[i].C,
-                       Allocation[i].A, Allocation[i].B, Allocation[i].C,
-                       Work.A + Allocation[i].A, Work.B + Allocation[i].B, Work.C + Allocation[i].C,
-                       "True");
-                cout << "|-------+-----------+-----------+-----------+-----------+--------|" << endl;
-                // end
-                
-                Work.A += Allocation[i].A;
-                Work.B += Allocation[i].B;
-                Work.C += Allocation[i].C;
-                
-                safe[securityNumber] = i;
-                securityNumber += 1;
-                i = -1;// 重新进行遍历
-            }
-        }
-    }
-    
-    // 如果所有进程的 Finish 向量都为 true 则处于安全状态，否则为不安全状态
-    for (int i = 0; i < NumberOfProcesses; i++) {
-        if (Finish[i] == false) {
-            
-            // 以下用以输出安全序列表格中的 false 行, 可单独注释掉
-            // start
-            printf( "| P%d    | %d   %d   %d | %d   %d   %d | %d   %d   %d | %d   %d   %d |  %s  |\n",
-                   i,
-                   Work.A, Work.B, Work.C,
-                   Need[i].A, Need[i].B, Need[i].C,
-                   Allocation[i].A, Allocation[i].B, Allocation[i].C,
-                   0, 0, 0,
-                   "False");
-            cout << "|-------+-----------+-----------+-----------+-----------+--------|" << endl;
-            // end
-            
-            cout << "当前系统处于不安全状态。" << endl;
-            return false;
-        }
-    }
-    cout << "当前系统处于安全状态。" << endl;
-    printf("安全序列为 { P%d, P%d, P%d, P%d, P%d }。\n", safe[0], safe[1], safe[2], safe[3], safe[4]);
-    return true;
-}
-
-//资源分配请求
-bool request(int process, Resource resource) {
-    // request 向量需小于Need矩阵中对应的向量
-    if(resource.A <= Need[process].A && resource.B <= Need[process].B && resource.C <= Need[process].C) {
-        // request 向量需小于Available向量
-        if(resource.A <= Available.A && resource.B <= Available.B && resource.C <= Available.C) {
-            // 试探分配
-            tryAllocate(process,resource);
-            // 安全检查, 如果安全则完成本次请求
-            if(safetyCheck()) {
-                return true;
-            } else {
-                // 否则将试探分配资源回滚
-                cout << "安全性检查失败。原因：系统将进入不安全状态，可能引起死锁。" << endl;
-                rollBack(process, resource);
-            }
-        } else {
-            cout << "安全性检查失败。原因：请求向量大于可利用资源向量。" << endl;
-        }
-    } else {
-        cout << "安全性检查失败。原因：请求向量大于需求向量。" << endl;
-    }
-    return false;
-}
-
-// 当前时刻输出资源分配表
-void printCurrentStatusTable()
-{
-    cout << "当前时刻资源分配情况: " << endl;
-    cout << "+--------------------------------------------------------+" << endl;
-    cout << "|       |    Max    |Allocation |    Need   | Available  |" << endl;
-    cout << "|Process|-----------+-----------+-----------+------------|" << endl;
-    cout << "|       | A   B   C | A   B   C | A   B   C | A   B   C  |" << endl;
-    cout << "|-------+-----------+-----------+-----------+------------|" << endl;
-    
-    printf( "| P0    | %d   %d   %d | %d   %d   %d | %d   %d   %d | %d   %d   %d  |\n", Max[0].A,Max[0].B,Max[0].C,Allocation[0].A,Allocation[0].B,Allocation[0].C,Need[0].A,Need[0].B,Need[0].C,Available.A,Available.B,Available.C);
-    cout << "|-------+-----------+-----------+-----------+------------|" << endl;
-    
-    for (int i = 1; i < NumberOfProcesses; i++) {
-        printf( "| P%d    | %d   %d   %d | %d   %d   %d | %d   %d   %d |            |\n",
-               i,
-               Max[i].A, Max[i].B, Max[i].C,
-               Allocation[i].A, Allocation[i].B, Allocation[i].C,
-               Need[i].A, Need[i].B, Need[i].C);
-        cout << "|-------+-----------+-----------+-----------+------------|" << endl;
-    }
-    cout << endl;
-}
 
 int main() {
     printCurrentStatusTable();
@@ -173,13 +17,7 @@ int main() {
     while (1) {
         int process;
         Resource resource;
-        
-        process = 0;
-        resource.A = 1;
-        resource.B = 1;
-        
-        // 取消以下注释以从键盘输入请求资源函数的参数
-        /*
+
         cout << "输入请求资源的进程下标(如0表示P0): ";
         cin >> process;
         cout << "输入请求资源的数量: " << endl;
@@ -189,7 +27,6 @@ int main() {
         cin >> resource.B;
         cout << "C: ";
         cin >> resource.C;
-        */
         
         if (request(process, resource))
         {
